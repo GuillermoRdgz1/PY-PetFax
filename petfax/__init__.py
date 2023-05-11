f938a52a838a01920763b14f8e43ceb62422537c
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-05-10 20:43:25 -0700

    databases

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask 
+from flask_migrate import Migrate
+
 
 def create_app(): 
     app = Flask(__name__)
+
+    # factory 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Memito1920!@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello(): 
@@ -16,3 +26,4 @@ def create_app():
     app.register_blueprint(facts.bp)
 
     return app
+
